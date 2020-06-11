7c0dd757906bd07279a79380be757751af7af32e
MoritzWag <moritzwagner95@hotmail.de>
2020-06-11 09:00:57 +0000

    started working on evaluator

---
+++ library/library/eval_helpers.py
@@ -0,0 +1,43 @@
+import numpy as np 
+import pandas as pd 
+import sklearn as sk 
+from sklearn.metrics import mutual_info_score
+
+
+def make_discretizer():
+    pass
+
+def discrete_mutual_info(zs, ys):
+    """Computes discrete mutual information
+    """
+    num_codes = zs.shape[1]
+    num_factors = ys.shape[1]
+
+    m = np.zeros([num_codes, num_factors])
+    for i in range(num_codes):
+        for j in range(num_factors):
+            m[i, j] = mutual_info_score(ys[:, j], zs[:, i])
+    
+    return m 
+
+def discrete_entropy(zs):
+    """Computes discretized entropy
+    """
+    num_factors = zs.shape[1]
+    h = np.zeros(num_factors)
+    for j in range(num_factors):
+        h[j] = mutual_info_score(zs[:, j], zs[:, j])
+    
+    return h 
+
+def histogram_discretize(target, num_bins):
+    """Discretiation based on histograms
+    """
+    discretized = np.zeros_like(target)
+    for i in range(target.shape[1]):
+        discretized[:, i] = np.digitize(target[:, i],
+                                np.histogram(
+                                    target[:, i], num_bins
+                                )[1][:, -1])
+    
+    return discretized
--- library/library/evaluator.py
+++ library/library/evaluator.py
@@ -1,7 +1,10 @@
 import pandas as pd 
 import numpy as np 
 import sklearn as sk 
+from sklearn.neighbors import KNeighborsRegressor
+import scipy 
 
+from library.eval_helpers import make_discretizer, discrete_mutual_info
 
 class Evaluator(nn.Module):
     """
@@ -9,10 +12,63 @@ class Evaluator(nn.Module):
     def __init__(self, **kwargs):
         super(Evaluator, self).__init__(**kwargs)
 
-    def _knn_prediction(self, data):
+    def _knn_regressor(self, data):
+
+        zs = []
+        for batch, (image, attribute) in enumerate(data):
+            h_enc = self.img_encoder(image.float())
+            z = self._reparameterization(h_enc)
+            z = z.cpu().detach().cpu()
+            zs.append(z)
+            
+            # work with the labels here as well!
+
         
         ## train
 
         ## predict 
 
         ## evaluate
+    
+    def unsupervised_metrics(self, data):
+        """
+        """
+        scores = {}
+        
+        zs = []
+        for batch, (image, attribute) in enumerate(data):
+            h_enc = self.img_encoder(image.float())
+            z = self._reparameterization(h_enc)
+            z = z.cpu().detach().cpu()
+            zs.append(z)
+        
+        zs = np.stack(zs)
+        zs = np.squeeze(zs)
+
+        num_latents = zs.shape[1]
+        cov_zs = np.cov(zs)
+
+        scores['gaussian_total_correlation'] = gaussian_total_correlation(cov_zs)
+        scores['gaussian_wasserstein_correlation'] = guassian_wasserstein_correlation(cov_zs)
+        scores['gaussian_wasserstein_correlation_norm'] = (
+                scores['gaussian_wasserstein_correlation'] / np.sum(np.diag(cov_zs)))
+
+
+        zs_discrete = make_discretizer(zs)
+        mutual_info_matrix = discrete_mutual_info(mus_discrete, mus_discrete)
+        np.fill_diagonal(mutual_info_matrix, 0)
+        mutual_info_score = np.sum(mutual_info_matrix) / (num_latents**2 - num_latents)
+        scores['mutual_info_score'] = mutual_info_score
+
+        return scores 
+
+    def gaussian_total_correlation(self, cov):
+        """
+        """
+        return 0.5 * (np.sum(np.log(np.diag(cov))) - np.linalg.slogdet(cov)[1])
+    
+    def gaussian_wasserstein_correlation(self, cov):
+        """
+        """
+        sqrtm = scipy.linalg.sqrtm(cov * np.expand_dims(np.diag(cov), axis=1))
+        return 2 * np.trace(cov) - 2 * np.trace(sqrtm)
